c189a48d320703741d31875f12b9531be62df52d
Rui Diniz <rui.maiodiniz@hotmail.com>
2025-09-06 12:48:11 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import NotFound from "./pages/NotFound"; // cria este ficheiro
+import Homepage from "./Pages/Homepage.jsx";
+import NotFound from "./Pages/NotFound.jsx"; // cria este ficheiro
 
 function App() {
   return (
